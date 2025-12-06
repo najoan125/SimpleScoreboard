9989a3323cece307c0e3780e16f069c88498b7ec
najoan125 <tiaaa0012@gmail.com>
2025-12-06 22:30:04 +0900

    Refactor text formatting utility and update scoreboard configuration

--- src/main/kotlin/kr/hyfata/simplescoreboard/util/TextFormatUtil.kt
+++ src/main/kotlin/kr/hyfata/simplescoreboard/util/TextFormatUtil.kt
@@ -1,6 +1,5 @@
 package kr.hyfata.simplescoreboard.util
 
-import kr.hyfata.simplescoreboard.SimpleScoreboard
 import kr.hyfata.simplescoreboard.config.ConfigManager
 import me.clip.placeholderapi.PlaceholderAPI
 import org.bukkit.entity.Player
@@ -8,17 +7,14 @@ import org.bukkit.entity.Player
 object TextFormatUtil {
     fun getFormattedText(player: Player, text: String): String {
         val placeholders = mapOf(
-            "%world%" to { getFormattedWorld(player.world.name) ?: "" },
+            "%world%" to { getFormattedWorld(player.world.name) },
         )
 
         var result = getFormattedText(text) // 기존 getFormattedText(text: String) 함수를 호출하여 기본 서식 적용
         for ((placeholder, valueProvider) in placeholders) {
             result = result.replace(placeholder, valueProvider())
         }
-        return if (SimpleScoreboard.placeholderApiEnabled)
-            PlaceholderAPI.setPlaceholders(player, result)
-        else
-            result
+        return PlaceholderAPI.setPlaceholders(player, result)
     }
 
     fun getFormattedText(text: String): String {
@@ -26,7 +22,7 @@ object TextFormatUtil {
             .replace("\\\n", "")
     }
 
-    fun getFormattedWorld(world: String): String? {
-        return ConfigManager.scoreboardConfig.getString("worlds.$world", world)
+    fun getFormattedWorld(world: String): String {
+        return getFormattedText(ConfigManager.scoreboardConfig.getString("worlds.$world", world)!!)
     }
 }
